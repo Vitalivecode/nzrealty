de90c7a85cf1511fe9062191f81e7d0a1896c00d
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-20 17:46:26 +0530

    changed minros

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$ark_root  = "https://".$_SERVER['HTTP_HOST']."/vitadev/";
-echo $ark_root;
+$ark_root  = "https://".$_SERVER['HTTP_HOST'];
+echo $ark_root.'@';
 $ark_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-echo $ark_root;
+echo $ark_root.'@';
 $config['base_url'] = $ark_root;
+echo $ark_root;
 $config['admin_url'] = $ark_root.'admin/';
 
 /*
