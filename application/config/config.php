a0cf371355470b82d8f5177f9e9834e7a1a3b205
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-20 17:31:18 +0530

    baseurl changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$ark_root  = "https://".$_SERVER['HTTP_HOST'];
-$ark_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $ark_root;
+// $ark_root  = "https://".$_SERVER['HTTP_HOST'];
+// $ark_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "https://rentallistings.co.nz";
 $config['admin_url'] = $ark_root.'admin/';
 
 /*
