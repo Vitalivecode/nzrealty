c537c913a09226c6e8bd4f005d974605696e074c
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-20 15:05:48 +0530

    ark_root changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$ark_root  = "https://".$_SERVER['HTTP_HOST'];
+// $ark_root  = "https://".$_SERVER['HTTP_HOST'];
+$ark_root  = "https://rentallistings.co.nz/vitadev";
 $ark_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $ark_root;
 $config['admin_url'] = $ark_root.'admin/';
