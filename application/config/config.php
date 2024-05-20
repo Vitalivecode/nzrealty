33916a70576851bc00118a4104b8f72ae9196e51
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-20 16:29:20 +0530

    files htaccess and config changed

--- application/config/config.php
+++ application/config/config.php
@@ -39,7 +39,7 @@ $config['admin_url'] = $ark_root.'admin/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
