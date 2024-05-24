5d131a868938d625b8205401f3edbf4578e01e3f
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-24 16:16:39 +0530

    index_page removed

--- application/config/config.php
+++ application/config/config.php
@@ -38,7 +38,7 @@ $config['admin_url'] = $ark_root.'admin/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
