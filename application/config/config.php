2966e41b62b6b8f58f93999f22eb9b10f038510f
stoswebsolutions <stoswebsolutions@gmail.com>
2024-05-28 12:11:11 +0530

    del htaccess change condifg

--- application/config/config.php
+++ application/config/config.php
@@ -38,7 +38,7 @@ $config['admin_url'] = $ark_root.'admin/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
