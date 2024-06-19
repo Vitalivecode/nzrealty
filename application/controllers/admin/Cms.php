f3fe96c6b5e8dd6ed4a64e36f0eee92104b5b260
stoswebsolutions <stoswebsolutions@gmail.com>
2024-06-19 12:24:50 +0530

    recheck render

--- application/controllers/admin/Cms.php
+++ application/controllers/admin/Cms.php
@@ -445,7 +445,7 @@ class Cms extends CI_Controller {
 			if($tablename != 'settings')
 			    $gps->highlight_row('status','=','0','#f2616187');
             $data['output'] = $gps->render();
-			var_dump($gps->render());exit;
+			var_dump($gps->render());
             $this->load->view(ADMIN . 'edit',$data);
         }
         else
